4ff72a79f71b01c01b1856992d9aff27a638310f
root <haoyu.w@alibaba-inc.com>
2025-07-20 16:02:48 +0800

    add update

--- Test_FILE/workspace_human_eval/generated_workflows/humaneval/humaneval_0.py
+++ Test_FILE/workspace_human_eval/generated_workflows/humaneval/humaneval_0.py
@@ -22,44 +22,16 @@ class Workflow:
         """
         solution1 = await self.code_generate(instruction="Can you analyze this problem step by step and generate the code?")
         solution2 = await self.code_generate(instruction="Can you analyze this problem step by step and generate the code?")
-        solution3 = await self.code_generate(instruction="Can you analyze this problem step by step and generate the code?")
-
-        test_result1 = await self.code_runner(solution=solution1)
-        test_result2 = await self.code_runner(solution=solution2)
-        test_result3 = await self.code_runner(solution=solution3)
-
-        solutions = []
-        if test_result1 == "PASSED":
-            solutions.append(solution1)
-        if test_result2 == "PASSED":
-            solutions.append(solution2)
-        if test_result3 == "PASSED":
-            solutions.append(solution3)
-
-        if len(solutions) > 0:
-            ensembled_solution = await self.sc_ensemble(solutions=solutions)
-            reviewed_solution = await self.review(solution=ensembled_solution)
-            return reviewed_solution
+        
+        reviewed_solution1 = await self.review(solution=solution1)
+        reviewed_solution2 = await self.review(solution=solution2)
+        
+        ensembled_solution = await self.sc_ensemble(solutions=[reviewed_solution1, reviewed_solution2])
+        
+        test_result = await self.code_runner(solution=ensembled_solution)
+        
+        if test_result == "PASSED":
+            return ensembled_solution
         else:
-            fixed_solution1 = await self.code_fix(solution=solution1, error_message=test_result1)
-            fixed_solution2 = await self.code_fix(solution=solution2, error_message=test_result2)
-            fixed_solution3 = await self.code_fix(solution=solution3, error_message=test_result3)
-
-            test_result_fixed1 = await self.code_runner(solution=fixed_solution1)
-            test_result_fixed2 = await self.code_runner(solution=fixed_solution2)
-            test_result_fixed3 = await self.code_runner(solution=fixed_solution3)
-
-            fixed_solutions = []
-            if test_result_fixed1 == "PASSED":
-                fixed_solutions.append(fixed_solution1)
-            if test_result_fixed2 == "PASSED":
-                fixed_solutions.append(fixed_solution2)
-            if test_result_fixed3 == "PASSED":
-                fixed_solutions.append(fixed_solution3)
-
-            if len(fixed_solutions) > 0:
-                ensembled_fixed_solution = await self.sc_ensemble(solutions=fixed_solutions)
-                reviewed_fixed_solution = await self.review(solution=ensembled_fixed_solution)
-                return reviewed_fixed_solution
-            else:
-                return "No valid solution found."
+            fixed_solution = await self.code_fix(solution=ensembled_solution, error_message=test_result)
+            return fixed_solution
